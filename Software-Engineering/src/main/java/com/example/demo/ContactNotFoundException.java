63bc29121c415754f1a94fb45a74c8db52a86097
Garrett Myers <gbmyers01@gmail.com>
2024-12-19 21:35:45 -0600

    Added yet again.

--- Software-Engineering/src/main/java/com/example/demo/ContactNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-class ContactNotFoundException extends RuntimeException {
-
-    ContactNotFoundException(Long id) {
-        super("Could not find employee " + id);
-    }
-}
--- Software-Engineering/src/main/java/com/example/demo/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface ContactRepository extends JpaRepository<Contact, Long> {
-
-}
